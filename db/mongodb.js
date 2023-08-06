c641d4b86f7a1aeb5e0078076faabb8ea544ea20
SamuelWeinberg <133872209+SamuelWeinberg@users.noreply.github.com>
2023-08-06 14:11:07 +0300

    Update mongodb.js

--- db/mongodb.js
+++ db/mongodb.js
@@ -6,4 +6,3 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/products');
   console.log('ok')
 }
-// await mongoose.connect('mongodb+srv://Samuel:fnLZMB5WZpHLgebV@atlascluster.ri55fhf.mongodb.net/products');
